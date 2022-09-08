fa2f6b56b6f0de2549c6ff13a3ffa32266c897e8
Thomas Brockington <xtombrock1989@gmail.com>
2022-09-08 21:20:22 +0100

    fin

--- src/token.js
+++ src/token.js
@@ -1,34 +1,35 @@
-const jwt = require('jsonwebtoken');
-
-/**
- * Implement this function to accept a payload and a secret key and return a JWT without an expiry time
- * 
- * Documentation: https://www.npmjs.com/package/jsonwebtoken#jwtsignpayload-secretorprivatekey-options-callback
- */
-function createToken() {
-    
+const jwt = require("jsonwebtoken");
+// takes params of payload and secret key, could add header data
+function createToken(payload, secret) {
+    return jwt.sign(payload, secret);
 }
+console.log(createToken({ user: 'tom', id:42 }, 'secretkey'));
+// this returns eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoidG9tIiwiaWQiOjQyLCJpYXQiOjE2NjI2NjcxMjV9.A-iX4EwZHxQbgzfStQeo5tHgSqOWI9DwvOQ_KBoyJAM
 
-/**
- * Implement this function to accept a payload, secret key and an expiry time, and return a JWT with an expiry
- * 
- * Documentation: https://www.npmjs.com/package/jsonwebtoken#token-expiration-exp-claim
- */
-function createTokenWithExpiry() {
-    
+
+
+function createTokenWithExpiry(payload, secret, expiryTime) {
+  return jwt.sign(payload, secret, { expiresIn: expiryTime})
 }
+console.log(createTokenWithExpiry({user: 'tom', id: 42 }, 'secretkey', '24h'));
+// returns eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoidG9tIiwiaWQiOjQyLCJpYXQiOjE2NjI2Njc1MzAsImV4cCI6MTY2Mjc1MzkzMH0.v5eVoEp9oSNSdmXc_vWqeH5t7OpriKnqOP4qTkQLERg
 
-/**
- * Implement this function to accept a JWT and a secret key. Return the decoded token (the payload) if verification is successful, and false if it fails
- * 
- * Documentation: https://www.npmjs.com/package/jsonwebtoken#jwtverifytoken-secretorpublickey-options-callback
- */
-function verifyToken() {
 
+function verifyToken(token, secretkey) {
+  try {
+    return jwt.verify(token, secretkey)
+  } catch (error) {
+    return false
+  }
 }
+const token = createTokenWithExpiry({user: 'tom', id: 42 }, 'secretkey', '24h')
+console.log(token);
+
+console.log(verifyToken(token, 'secretkey'));
+console.log(verifyToken(token, 'somethingelse'));
 
 module.exports = {
-    createToken,
-    createTokenWithExpiry,
-    verifyToken
-}
+  createToken,
+  createTokenWithExpiry,
+  verifyToken,
+};
